1247f64eb30db26b7d3cc5820431d4d961bc5b6f
GMax009 <gratis.maxwell@gmail.com>
2023-04-18 13:25:12 -0400

    added basic hit explosion to player and enemy projectiles

--- WastewaterRoundup/Assets/Scripts/PlayerAttackShoot.cs
+++ WastewaterRoundup/Assets/Scripts/PlayerAttackShoot.cs
@@ -31,4 +31,4 @@ public class PlayerAttackShoot : MonoBehaviour{
             GameObject projectile = Instantiate(projectilePrefab, this.transform.position, Quaternion.identity);
             projectile.GetComponent<Rigidbody2D>().AddForce(fwd * projectileSpeed, ForceMode2D.Impulse);
       }
-}
+} 
--- WastewaterRoundup/Assets/Scripts/PlayerProjectile.cs
+++ WastewaterRoundup/Assets/Scripts/PlayerProjectile.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerProjectile : MonoBehaviour{
 
-      public int damage = 1;
+      public int damage = 10;
       public GameObject hitEffectAnim;
       public float SelfDestructTime = 4.0f;
       public float SelfDestructVFX = 0.5f;
@@ -16,18 +16,18 @@ public class PlayerProjectile : MonoBehaviour{
       }
 
       //if the bullet hits a collider, play the explosion animation, then destroy the effect and the bullet
-     /* void OnTriggerEnter2D(Collider2D other){
-            //if (other.gameObject.layer == LayerMask.NameToLayer("Enemies")) {
+      void OnTriggerEnter2D(Collider2D other){
+            if (other.gameObject.layer == LayerMask.NameToLayer("Enemies")) {
                   //gameHandlerObj.playerGetHit(damage);
-                  //other.gameObject.GetComponent<EnemyLives>().TakeDamage(damage);
-            //}
+                  other.gameObject.GetComponent<EnemyMeleeDamage>().TakeDamage(damage);
+            }
            if (other.gameObject.tag != "Player") {
                   GameObject animEffect = Instantiate (hitEffectAnim, transform.position, Quaternion.identity);
-				  projectileArt.enabled = false;
-                  Destroy (animEffect, 0.5);
+                  projectileArt.enabled = false;
+                  //Destroy (animEffect, 0.5);
                   StartCoroutine(selfDestructHit(animEffect));
             }
-      }*/
+      }
 
       IEnumerator selfDestructHit(GameObject VFX){
             yield return new WaitForSeconds(SelfDestructVFX);
