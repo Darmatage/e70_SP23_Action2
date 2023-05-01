496ad884880a69908ab69768f0d88b48a362bacb
Darmatage <JasonWiserArt@gmail.com>
2023-05-01 13:23:23 -0400

    New Tilemap for pipes and dirt, new fish carcass and bubbles. Also, NPC dialogue system for tutorial!

--- WastewaterRoundup/Assets/Scripts/CameraController.cs
+++ WastewaterRoundup/Assets/Scripts/CameraController.cs
@@ -10,18 +10,18 @@ public class CameraController : MonoBehaviour{
 	  public float damping = 0.69f;
 	  
 	  private Vector3 velocity = Vector3.zero;
-      //public float camSpeed = 4.0f;
+      public float camSpeed = 4.0f;
 
       void Start(){
             target = GameObject.FindWithTag("Player");
       }
 
       void Update () {
-			Vector3 movePosition = target.transform.position + offset;
-			transform.position = Vector3.SmoothDamp(transform.position, movePosition, ref velocity, damping);
+			//Vector3 movePosition = target.transform.position + offset;
+			//transform.position = Vector3.SmoothDamp(transform.position, movePosition, ref velocity, damping);
 			
-			//Vector2 pos = Vector2.Lerp ((Vector2)transform.position, (Vector2)target.transform.position, camSpeed * Time.fixedDeltaTime);
-            //transform.position = new Vector3 (pos.x, pos.y, transform.position.z);
+			Vector2 pos = Vector2.Lerp ((Vector2)transform.position, (Vector2)target.transform.position, camSpeed * Time.fixedDeltaTime);
+            transform.position = new Vector3 (pos.x, pos.y, transform.position.z);
       }
 }
 
