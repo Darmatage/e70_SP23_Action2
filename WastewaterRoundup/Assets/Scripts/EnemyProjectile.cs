dfff542165e10b9e611093849b6ea9399c411ffd
PBard83 <PBard83@users.noreply.github.com>
2023-05-06 19:25:44 -0400

    Added sounds and animations
    Added banjo sound
    Added spawn sound
    Changed enemy projectiles
    Changed player projectiles (added animation, looks much cooler now)

--- WastewaterRoundup/Assets/Scripts/EnemyProjectile.cs
+++ WastewaterRoundup/Assets/Scripts/EnemyProjectile.cs
@@ -38,9 +38,11 @@ public class EnemyProjectile : MonoBehaviour {
 
        //if the bullet hits a collider, play the explosion animation, then destroy the effect and the bullet
        void OnTriggerEnter2D(Collider2D collision){
-              if (collision.gameObject.tag == "Player") {
+              if (collision.gameObject.tag == "Player")
+				  soundEffect.Play();
+			  {
                      gameHandlerObj.playerGetHit(damage);
-					 soundEffect.Play();
+
               }
               if (collision.gameObject.tag != "enemyShooter") {
                      GameObject animEffect = Instantiate (hitEffectAnim, transform.position, Quaternion.identity);
