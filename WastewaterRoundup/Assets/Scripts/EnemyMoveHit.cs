352e39f065c8fa5a1b5d5082ddef198507042d07
rishabhchopra1096 <rishabhud2@yahoo.com>
2023-05-11 19:54:24 +0530

    added small chaser idle and chase animation

--- WastewaterRoundup/Assets/Scripts/EnemyMoveHit.cs
+++ WastewaterRoundup/Assets/Scripts/EnemyMoveHit.cs
@@ -40,7 +40,7 @@ public class EnemyMoveHit : MonoBehaviour {
 
               if ((target != null) && (DistToPlayer <= attackRange)){
                      transform.position = Vector2.MoveTowards (transform.position, target.position, speed * Time.deltaTime);
-                    //anim.SetBool("Walk", true);
+                    anim.SetBool("chasing", true);
                     //flip enemy to face player direction. Wrong direction? Swap the * -1.
                     if (target.position.x > gameObject.transform.position.x){
                                    gameObject.transform.localScale = new Vector2(scaleX, gameObject.transform.localScale.y);
@@ -48,7 +48,7 @@ public class EnemyMoveHit : MonoBehaviour {
                                     gameObject.transform.localScale = new Vector2(scaleX * -1, gameObject.transform.localScale.y);
                     }
               }
-               //else { anim.SetBool("Walk", false);}
+               else { anim.SetBool("chasing", false);}
        }
 
        public void OnCollisionEnter2D(Collision2D other){
